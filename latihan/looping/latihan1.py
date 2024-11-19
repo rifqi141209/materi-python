d720523710a36d3c486d1e296299d23c4cd001dd
rifqi141209 <pone55241@gmail.com>
2024-11-19 07:54:55 +0700

    menambah perubahan

---
+++ latihan/looping/latihan1.py
@@ -0,0 +1,12 @@
+#program cetak teks sebanyak 100 kali dengan looping 
+#inisiasi variabel perulangan 
+x = 1
+
+#perilangan sebanyak 100 kali
+while x <= 100:
+    #cetak teks
+    print("Saya anka smk binbina kerja")
+    print("kelas x-RPL")
+    
+    #tambahkan x dengan 1 setiap perulangan 
+    x+=10
